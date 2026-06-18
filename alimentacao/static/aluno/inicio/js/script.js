520a4c035622eec14038166ec3fb70a5efa06437
Satiro <satirojose492@gmail.com>
2026-06-18 14:20:36 -0300

    adiciona cores para o tema escuro

--- alimentacao/static/aluno/inicio/js/script.js
+++ alimentacao/static/aluno/inicio/js/script.js
@@ -1,15 +1,19 @@
 const btn_modo = document.querySelector('.btn_tema')
 
-const main = document.querySelector('main')
+const tema = document.documentElement
+
+const btn_modo_img = document.querySelector('.img_tema')
 
 let aux = false;
 
 btn_modo.addEventListener('click', () => {
     if (aux == false) {
         aux = true
-        main.classList.add('mainEscuro')
+        tema.dataset.theme = 'dark'
+        btn_modo_img.src = '/static/aluno/inicio/img/header/icons8-logout-100.png'
     } else {
         aux = false
-        main.classList.remove('mainEscuro')
+        tema.dataset.theme = 'light'
+        btn_modo_img.src = '/static/aluno/inicio/img/header/moon-svgrepo-com.svg'
     }
 })
