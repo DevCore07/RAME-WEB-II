940eb60cf9c3bb81f10db398ddc24e0384bd24cf
José Satiro <jardelsatiro57@gmail.com>
2026-06-26 17:09:05 -0300

    adiciona funcionalidade de restrições do aluno

--- alimentacao/views.py
+++ alimentacao/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login, authenticate
 
+
+dicionario_restricoes = [
+    {
+        "restricao": "Intolerância à Lactose",
+        "status": "ativado",
+        "valor": "1"
+    },
+    {
+        "restricao": "Intolerância ao Glúten (Celíaco)",
+        "status": "desativado",
+        "valor": "2"
+    },
+    {
+        "restricao": "Alergias à castanhas/amemdoim",
+        "status": "desativado",
+        "valor": "3"
+    },
+    {
+        "restricao": "Alergias à frutos do mar",
+        "status": "desativado",
+        "valor": "4"
+    },
+    {
+        "restricao": "Alergia à Ovos",
+        "status": "desativado",
+        "valor": "5"
+    },
+    {
+        "restricao": "Alergia à sojja",
+        "status": "desativado",
+        "valor": "6"
+    }
+]
+
+
 # Create your views here.
 
 def login(request):
@@ -24,10 +59,18 @@ def login(request):
     return render(request, 'login/login.html')
 
 def inicio(request):
-    return render(request, 'aluno/inicio/inicio.html')
+    return render(request, 'aluno/inicio/inicio.html', context={'lista': dicionario_restricoes})
 
 def restricoes(request):
-    return render(request, 'aluno/restricoes/restricoes.html')
+    if request.method == 'POST':
+        valor = request.POST.getlist('restricao')
+        for rest in dicionario_restricoes:
+            if rest['valor'] not in valor:
+                rest['status'] = 'desativado'
+            else:
+                rest['status'] = 'ativado'
+                
+    return render(request, 'aluno/restricoes/restricoes.html', context={'lista': dicionario_restricoes})
 
 
 def cardapio(request): 
